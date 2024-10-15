b9ca0d346ee959692e1cac6b6dffcb3467938c40
Tahsin411934 <tahsin.mail.0503@gmail.com>
2024-10-15 23:23:46 +0600

    basic structure and package added

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+      <div>
+        <h1 className='bg-red-500'>Welcome to ShareTrip!</h1>
+        <p>Share your dreams with others, plan trips, and discover new places.</p>
+        this is home page
+      </div>
+    )
+ 
+}
+
+export default Home
